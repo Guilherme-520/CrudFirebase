70911b5e95313360a9e599298485a64381da1001
Guilherme-520 <guilhermecordeiro520@outlook.com>
2023-06-28 15:57:30 -0300

    crud completo firebase

--- app.js
+++ app.js
@@ -23,19 +23,42 @@ app.get("/", function (req, res) {
 })
 
 app.get("/consulta", async function (req, res) {
-    const snapshot = await db.collection('agendamentos').get();
-    snapshot.findAll().then(function(doc){
-        const post = doc.data()
-        res.render("consulta", {post})
-    }).catch(function(erro){
-        console.log("Erro ao carregar dados do banco: " + erro)
+    await db.collection('agendamentos').get().then((snapShot)=>{
+        const doc = [];
+        snapShot.forEach((docs)=>{
+            doc.push({id: docs.id});
+            doc.push(docs.data());
+        })
+        res.render("Consulta", {doc})
+        console.log(doc)
+    }).catch((error)=>{
+        console.log(error);
     })
 })
 
-app.get("/editar/:id", function (req, res) {
+app.get("/editar/:id", async function (req, res) {
+    const id = req.params.id
+    const doc = await db.collection("agendamentos").doc(`${id}`).get()
+    const post = []
+    try {
+        post.push(id)
+        post.push(doc.data())
+        console.log(post)
+        res.redirect("editar", {post})
+    } catch (error) {
+        console.error(error)
+    }
+
 })
 
-app.get("/excluir/:id", function (req, res) {
+app.get("/excluir/:id", async function (req, res) {
+    const id = req.body.id;
+    await db.collection('agendamentos').doc(`${id}`).delete().then(()=>{
+        res.redirect("/");
+    }).catch((error)=>{
+        console.log(error)
+    });
+   
 })
 
 app.post("/cadastrar", function (req, res) {
@@ -51,7 +74,17 @@ app.post("/cadastrar", function (req, res) {
     })
 })
 
-app.post("/atualizar", function (req, res) {
+app.post("/atualizar", async function (req, res) {
+    await db.collection("agendamentos").doc(req.body.id).update({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    }).then(function () {
+        console.log('Updated document');
+        res.redirect('/')
+    })
 })
 
 app.listen(8081, function () {
